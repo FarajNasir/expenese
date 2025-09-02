b0d28a9aef594562981dad48cfa962db403a0f88
FarajNasir <farajnasir2019@gmail.com>
2025-09-02 17:17:45 +0530

    backend

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,12 @@ import express, { Router } from 'express'
 import { loginController, registerController } from '../controller/user.controller.js'
 
 const router=Router()
+
+// POST || REGISTER
+router.route('/register').post(registerController)
 // POST || login
 // router.route("/login").post(loginController)
 router.post('/login',loginController)
 
-// POST || REGISTER
-router.route('/register').post(registerController)
+
 export default router
