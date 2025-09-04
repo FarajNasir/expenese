01f297f1d4cc3ddade8063036938e7bb855b0d74
FarajNasir <farajnasir2019@gmail.com>
2025-09-04 16:14:40 +0530

    backend done

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Form, Input ,message} from 'antd'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import Password from 'antd/es/input/Password'
+import Spinner from '../components/Spinner'
+
+const Login = () => {
+    const [loading,setLoading]=useState(false)
+    const navigate = useNavigate()
+    const submitHandler = async(values) => {
+        try {
+            setLoading(true)
+           const {data}= await axios.post('/api/v1/user/login', values)
+           
+           message.success("Login success");
+           setLoading(false)
+           navigate('/')
+           localStorage.setItem('user',JSON.stringify({...data,Password:""}))
+        } catch (error) {
+            setLoading(false)
+           message.error(error.response?.data?.message || "Something went wrong");
+        }
+    }
+    return (
+        <>
+            <div className='register'>
+                {loading && <Spinner/>}
+                <Form layout='vertical' onFinish={submitHandler}>
+                    <h1>Login Form</h1>
+                    <Form.Item label="Email" name="email">
+                        <Input type='email' />
+                    </Form.Item>
+                    <Form.Item label="Password" name="password">
+                        <Input type='password' />
+                    </Form.Item>
+                    <div className="d-flex">
+                        <Link to='/register'>Not a user ? Click Here to register</Link>
+                        <button className='btn btn-primary'>Login</button>
+                    </div>
+                </Form>
+            </div></>
+    )
+}
+
+export default Login
---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+import {Form,Input,message} from 'antd'
+import { Link,useNavigate } from 'react-router-dom'
+import Spinner from '../components/Spinner'
+
+const Register = () => {
+    const [loading,setLoading]=useState(false)
+    const navigate=useNavigate()
+    const submitHandler=async(values)=>{
+        try {
+            setLoading(true)
+            await axios.post('/api/v1/user/register',values)
+            message.success("Registration successful")
+            setLoading(false)
+            navigate('/login')
+        } catch (error) {
+            setLoading(false)
+            message.error("Invallid username or password");
+            
+        }
+    }
+  return (
+   <>
+    <div className='register'>
+        {loading && <Spinner/>}
+       <Form layout='vertical' onFinish={submitHandler}>
+        <h1>Register Form</h1>
+        <Form.Item label="Name" name="name">
+            <Input/>
+        </Form.Item>
+        <Form.Item label="Email" name="email">
+            <Input type='email'/>
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+            <Input type='password'/>
+        </Form.Item>
+        <div className="d-flex">
+            <Link to='/login'>Already Registered ? click here to login</Link>
+            <button className='btn btn-primary'>Register</button>
+        </div>
+       </Form>
+    </div></>
+  )
+}
+
+export default Register
